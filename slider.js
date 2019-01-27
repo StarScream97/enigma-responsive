0166f37875e19da11b82aeda47df65bc4d0968d9
StarScream97 <9tyninelives@gmail.com>
2019-01-27 13:39:56 +0545

    Basic Responsive Working Perfectly With The Exception of Typography and Element Sizings

---
+++ slider.js
@@ -0,0 +1,27 @@
+// const sliderFor=document.getElementById('slider-for');
+// const gotoone=document.getElementById('gotoone');
+// const gototwo=document.getElementById('gototwo')
+// sliderFor.slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+//   });
+
+// const multipleItems=document.getElementsByClassName('multiple-items');
+// // multipleItems.slick()
+// multipleItems.slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
+
+// const sliderNav=document.getElementById('slider-nav')
+// sliderNav.slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   // asNavFor: '.slider-for',
+//   dots: true,
+//   focusOnSelect: true
+// });
